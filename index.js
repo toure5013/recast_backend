3a222e059eb246233a58c7f27d399459b7417cb1
TOURE <toure5013@gmail.com>
2019-08-22 11:26:27 +0000

    BISMILAH BACKEND

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+//Parameters
+
+//===========================Modules exports==========================
+var     
+http = require("http"),
+express = require("express"),
+app = express(),
+server = http.createServer(app),
+bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
+logger = require("morgan"),
+errorHandler = require("errorhandler"),
+
+//My libraries
+routes = require('./server/routes')
+;
+
+const nodePort = 8088 || process.env.PORT;
+
+//============================Configuration our server==================
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+switch (app.get('env')){
+    case 'development':
+        app.use(logger('combined'));
+        app.use(errorHandler(
+            {
+                dumExceptions : true,
+                showStack : true
+            }
+        ));//We can put production case
+}
+
+routes.configRoutes(app, server);
+
+
+
+//=============================Start server=========================
+server.listen(nodePort);
+console("port");
+console(nodePort);
+console.log(
+    'express server listening on port %d in %s mode',
+    nodePort,
+    app.settings.env
+);
